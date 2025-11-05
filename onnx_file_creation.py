84c0b800b84ee96bb1ae6e54bf66bad9078ce9f8
smassm <ali.sadaty8396@gmail.com>
2025-11-05 10:20:52 +0330

    Add files via upload
    change the image size in this code and make a new .onnx file in case you want to change the size in the main code(make sure to make changes in frame size in main code)

---
+++ onnx_file_creation.py
@@ -0,0 +1,29 @@
+# from ultralytics import YOLO
+#
+# model = YOLO(r"C:\softwares\yolov8_env64\runs\segment\window_segmentation_model8\weights\best.pt")
+#
+# model.export(
+#     format="onnx",
+#     imgsz=320,           # keep full input resolution
+#     opset=19,             # latest ONNX opset for precision
+#     dynamic=False,         # allows flexible image sizes
+#     simplify=False,       # keeps full computation graph (better precision)
+#     half=False            # ensures FP32, not FP16 , keep it False for CPU processing
+# )
+
+from ultralytics import YOLO
+
+from f2dummy2 import simplify
+
+model = YOLO(r"C:\softwares\yolov8_env64\runs\segment\window_segmentation_model8\weights\best.pt")
+
+model.export(
+    format="onnx",
+    imgsz=480,           # keep full input resolution
+    opset = 17,
+    dynamic= False,         # allows flexible image sizes
+    simplify= True,       # keeps full computation graph (better precision)
+    half= False
+
+)
+
